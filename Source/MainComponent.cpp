eac3a13a36c80030e16bb1dc36f61b7cf096dd19
logsol <logspum@gmail.com>
2018-05-25 10:05:05 +0200

    Initial version of autocorrelation based guitar tuner

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,121 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+    startTimer (50);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    g.setFont (Font (146.0f));
+    g.setColour (Colours::orange);
+    
+    float note = round(log(frequency / 440.0) / log(2) * 12 + 69);
+    juce::String noteName = MidiMessage::getMidiNoteName(note, true, false, 3);
+    
+    //if (noteName == "E") {
+        g.drawText (noteName, getLocalBounds(), Justification::centred, true);
+    //}
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
+
+void MainComponent::audioDeviceIOCallback (const float** inputChannelData, int numInputChannels,
+                            float** outputChannelData, int numOutputChannels, int numSamples)
+{
+    const ScopedLock sl (lock);
+    
+    // Todo: Could be slightly improved by recording into a ring buffer
+    // Todo: Play around with threshold to find a value for good detection
+    
+    for (int l = 0; l < numSamples; ++l) {
+        
+        if (count < recordSize) {
+            
+            // Record 2000 samples (only use left / first channel)
+            recordedSamples[count] = inputChannelData[0][l];
+            count++;
+            
+        } else {
+            
+            // After samples are recorded
+            sum = 0;
+            pdState = 0;
+            int period = 0;
+            
+            for (i=0; i < recordSize; i++) {
+                
+                // Autocorrelation
+                sumOld = sum;
+                sum = 0;
+                
+                for (k=0; k < recordSize-i; k++) {
+                    sum += recordedSamples[k] * recordedSamples[k+i];
+                }
+                
+                // Peak Detect State Machine
+                if (pdState == 2 && (sum-sumOld) <= 0) {
+                    period = i;
+                    pdState = 3;
+                }
+                
+                if (pdState == 1 && (sum > thresh) && (sum-sumOld) > 0) {
+                    pdState = 2;
+                }
+                
+                if (!i) {
+                    thresh = sum * 0.5;
+                    pdState = 1;
+                }
+            }
+
+            // Frequency identified in Hz
+            if (thresh > 10) {
+                frequency = sampleRate / period;
+            }
+            count = 0;
+            
+        }
+    }
+}
+
+void MainComponent::audioDeviceAboutToStart (AudioIODevice* device)
+{
+    sampleRate = device->getCurrentSampleRate();
+}
+
+void MainComponent::audioDeviceStopped()
+{
+    
+}
+
+void MainComponent::audioDeviceError (const String& errorMessage)
+{
+    std::cout << errorMessage;
+}
+
+void MainComponent::timerCallback()
+{
+    repaint();
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,57 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent   : public Component,
+                        public AudioIODeviceCallback,
+                        public Timer
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+    void audioDeviceIOCallback (const float** inputChannelData, int numInputChannels,
+                                float** outputChannelData, int numOutputChannels, int numSamples) override;
+    
+    void audioDeviceAboutToStart (AudioIODevice* device) override;
+    
+    void audioDeviceStopped() override;
+    
+    void audioDeviceError (const String& errorMessage) override;
+    
+    void timerCallback() override;
+
+private:
+    //==============================================================================
+    float frequency = 0;
+    float sampleRate;
+    int recordSize = 2000;
+    float recordedSamples[2000] = { };
+    int count = 0;
+    float sum;
+    float sumOld;
+    int i, k;
+    int pdState = 0;
+    int thresh = 0;
+    
+    CriticalSection lock;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
